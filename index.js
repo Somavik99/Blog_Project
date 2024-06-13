3fb33ebd15204eb9232013070c16b493b992dce3
Avik Roy <somavik99@gmail.com>
2024-06-13 16:26:06 +0530

    Connection build complete

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
-const app = express()
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT_URI || 8000;
+mongoose.set("strictQuery", false);
+
+async function ConnectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.log("DB connection failed: " + err.message);
+  }
+}
+
+app.use(express.json());
+app.use(cookieParser());
+
+// app.use("/api/v1/auth", authRoute);
+
+ConnectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("error found: " + err.message);
+  });
