632b4a4aee48d2d2a96076ce32412705612982be
Avik Roy <somavik99@gmail.com>
2024-06-18 17:39:04 +0530

    User restriction

--- Auth/varifyLoginTokens.js
+++ Auth/varifyLoginTokens.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../Models/UserSchema.js";
 
+/* Authenticating The token */
+
 export async function AuthenticateLoginToken(req, res, next) {
   const authToken = req.headers.authorization;
   console.log(authToken);
@@ -19,8 +21,10 @@ export async function AuthenticateLoginToken(req, res, next) {
     req.role = decodeToken.role;
     next();
   } catch (error) {
-    if(error.name === "TokenExpiredError"){
-      return res.status(401).json({success:false,message:"Token Expired😢...!"})
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ success: false, message: "Token Expired😢...!" });
     }
     res.status(401).json({
       success: false,
@@ -28,3 +32,35 @@ export async function AuthenticateLoginToken(req, res, next) {
     });
   }
 }
+
+// Restricting if not loggedOIn or signedIn
+
+export const restrictNonUser = (role) => async (req, res, next) => {
+  try {
+    const userId = role.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: true, message: "User not found...!" });
+    }
+
+    const userRole = user.role;
+    if (userRole === "user" || role.includes("user")) {
+      next();
+    } else if (userRole === "admin" || role.includes("admin")) {
+      next();
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "User not authorized...!" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: `Internal server error: ${error.message}`,
+      });
+  }
+};
